e7089c10106fec85cb1d4f76ede293827e73a138
Wouter Kouw <wmkouw@gmail.com>
2017-09-20 14:49:57 +0200

    Bug fix for file_utils and added environment.yaml for conda env create

--- quiver_engine/file_utils.py
+++ quiver_engine/file_utils.py
@@ -1,21 +1,35 @@
 import re
 import numpy as np
+import matplotlib as plt
 from os.path import relpath, abspath
 from os import listdir
 
+from quiver_engine.timeseries_visualization import plot_timeseries
+
 def save_layer_sig(layer_outputs, layer_name, idx, temp_folder, input_path):
 
-    # Find out where to write network layer outputs
-    filename = get_output_filename(layer_name, idx, temp_folder, input_path)
+    # Make visualization of layer ouput
+    time_plot = plot_timeseries(layer_outputs,dt=0.01)
+    time_plot['fig'].save_fig(get_output_png_filename(layer_name,
+                idx,
+                temp_folder,
+                input_path))
 
     # Save network layer outputs as numpy arrays
-    np.save(filename, layer_outputs)
+    np.save(get_output_npy_filename(layer_name,
+                idx,
+                temp_folder,
+                input_path),
+            layer_outputs)
 
     # Return path to layer output arrays
     return relpath(filename, abspath(temp_folder))
 
-def get_output_filename(layer_name, z_idx, temp_folder, input_path):
-    return '{}/{}_{}_{}.npy'.format(temp_folder, layer_name, str(z_idx), input_path)
+def get_output_npy_filename(layer_name, z_idx, temp_folder, input_path):
+    return '{}/{}_{}_{}.npy'.format(temp_folder, layer_name, str(z_idx), input_path[:-4])
+
+def get_output_png_filename(layer_name, z_idx, temp_folder, input_path):
+    return '{}/{}_{}_{}.png'.format(temp_folder, layer_name, str(z_idx), input_path[:-4])
 
 def list_sig_files(input_folder):
     image_regex = re.compile(r'.*\.(npy)$')
--- test_signal_win.py
+++ test_signal_win.py
@@ -3,7 +3,7 @@ import keras as ks
 from quiver_engine import server
 
 # Load model
-model = ks.models.load_model('models/my_bestmodel.h5')
+model = ks.models.load_model('models/my_bestmodel-win.h5')
 
 # Launch server
 server.launch(model, classes=range(7), input_folder='input/')
